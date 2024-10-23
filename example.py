c743d4ef9581712708c69d605e7b7d86dbd8452e
lbz <labeebbi@buffalo.edu>
2024-10-23 09:44:37 -0400

    updated github workflow

--- example.py
+++ example.py
@@ -9,17 +9,17 @@ key = os.getenv('RUFUS_API_KEY', 'default_key')
 client = RufusClient(api_key=key)
 
 url = 'https://www.taniarascia.com'
-instructions = "extract articles about javascript, react, web-development"
+instructions = "extract articles about javascript, react, web development"
 
 # Scrape the website
 documents = client.scrape(url, instructions)
 
-# Output the results
-output_folder = 'outputs'
-os.makedirs(output_folder, exist_ok=True)
-file_path = os.path.join(output_folder, 'testwebsite.json')
+# # Output the results
+# output_folder = 'outputs'
+# os.makedirs(output_folder, exist_ok=True)
+# file_path = os.path.join(output_folder, 'testwebsite.json')
 
-with open(file_path, 'w') as f:
-    json.dump(documents, f, indent=4)
+# with open(file_path, 'w') as f:
+#     json.dump(documents, f, indent=4)
 
-print(f"Data saved to {file_path}")
+# print(f"Data saved to {file_path}")
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rufus-ai',
-    version='0.1.0',
+    version='0.1.2',
     author='Labeeb Zahed',
     author_email='labeebzahed@gmail.com',
     description='Rufus: Intelligent Web Data Preparation for RAG Agents',
