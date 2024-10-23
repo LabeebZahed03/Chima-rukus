cd9a1681792be24bda8ecb40db4c7ba89bcb208e
lbz <labeebbi@buffalo.edu>
2024-10-23 09:51:49 -0400

    Bump version to 0.1.3 for PyPI release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rufus-ai',
-    version='0.1.2',
+    version='0.1.3',
     author='Labeeb Zahed',
     author_email='labeebzahed@gmail.com',
     description='Rufus: Intelligent Web Data Preparation for RAG Agents',
